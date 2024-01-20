081ca29d7929e96535956d5ddb7e16de13358caf
William Zhao7140 <Chipotleguacamolly@gmail.com>
2024-01-19 18:23:26 -0800

    asd

--- contact.py
+++ contact.py
@@ -1,41 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import ActionChains
-import time, random, csv, os
-import pyautogui
+from datetime import datetime
+from pynput.mouse import Button, Controller
+import time, pyautogui, random
 
-# VIRTUAL MACHINE
-
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-geolocation")
-chrome_options.add_argument("--remote-allow-origins=*")
-
-chrome_options.add_argument('user-data-dir=/Users/williamzhao/Library/Application Support/Google/Chrome/')
-chrome_options.add_argument("--profile-directory=Profile 1")
-chrome_options.add_argument("--start-maximized")
-service = Service(executable_path='/opt/homebrew/bin/chromedriver')
-chrome_options.add_extension(r'buzzstream.crx')
-
-
-driver = webdriver.Chrome(service=service, options=chrome_options)
-# driver.fullscreen_window()
-
-# Project Boating Safety
-google_search_operators = [
-    "https://www.google.com/search?q=boat-ed.com+inurl:resources",
-    "https://www.google.com/search?q=asdasd123"
-    "https://www.google.com/search?q=boating+safety+marinas+inurl:.org/resources"
-]
-
-action = ActionChains(driver)
+counter = 0
 
 def scroll_down():
     last_height = driver.execute_script("return document.body.scrollHeight")
@@ -43,58 +17,265 @@ def scroll_down():
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(2)
         new_height = driver.execute_script("return document.body.scrollHeight")
-
         if new_height == last_height:
             try:
-                element = WebDriverWait(driver, 1).until(
-                    EC.element_to_be_clickable((By.XPATH, '//*[@id="botstuff"]/div/div[3]/div[4]/a[1]/h3/div'))
+                element = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="botstuff"]/div/div[3]/div[4]/a[1]/h3/div'))
                 )
                 element.click()
             except Exception:
                 break
         last_height = new_height
-
-def highlight_contacts():
     
-    whitespace_element = driver.find_element(By.TAG_NAME, "body")
-    action.key_down(Keys.CONTROL).click(whitespace_element).key_up(Keys.CONTROL).perform()
-    for j in range(12):pyautogui.press('down')
-    pyautogui.press('return')
-    for j in range(3):pyautogui.press('down')
-    pyautogui.press('enter')
-    
-
-def get_all_urls():
+def get_filter_urls(min_da, max_da):
     urls = []
-    serp_listings = driver.find_elements(By.CLASS_NAME, "MjjYud")
+    serp_listings = WebDriverWait(driver, 5).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "MjjYud"))
+    )
     for listing in serp_listings:
         try:
-            urls.append(listing.find_element(By.TAG_NAME, 'a').get_attribute('href'))
-        except:
+            da_element = listing.find_element(By.XPATH, ".//div[contains(@class, 'ah_serpbar__item-inner')]/span[contains(@class, 'ah_serpbar__item-label') and text()='dr']/following-sibling::span[contains(@class, 'ah_serpbar__item-data')]")
+            da_number = float(da_element.text)
+            
+            if min_da <= da_number <= max_da:
+                listing_link = listing.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                if listing_link.endswith((".pdf", ".gov", ".edu")):
+                    continue
+                print(f"Domain Authority: {da_number} Link: {listing_link}")
+                urls.append(listing_link)
+        except Exception as e:
+            print(e)
             continue
     return urls
 
+def check_if_more_than_two_months(date_str, current_date=datetime.now()):
+    try:
+        # Handle case where date_str is in time format like '2:10 pm'
+        if ':' in date_str:
+            # Assuming the time is for the current date
+            date_obj = datetime.combine(current_date.date(), datetime.strptime(date_str, '%I:%M %p').time())
+        elif len(date_str) <= 6:
+            # Original logic for 'Jan 01' format
+            date_obj = datetime.strptime(date_str + ' ' + str(current_date.year), '%b %d %Y')
+            if date_obj > current_date:
+                date_obj = datetime.strptime(date_str + ' ' + str(current_date.year - 1), '%b %d %Y')
+        else:
+            # Original logic for 'mm/dd/yy' format
+            date_obj = datetime.strptime(date_str, '%m/%d/%y')
+            if date_obj.month > current_date.month or (date_obj.month == current_date.month and date_obj.day > current_date.day):
+                date_obj = date_obj.replace(year=date_obj.year - 1)
+        
+        return (current_date - date_obj).days >= 60
+    except ValueError:
+        return False
+    
+
+# IF NEED TO ADD PROJECT, ADD TO STREAM
+# CHECK IF RESEARCH PAGE URL EXISTS
 
 def check_and_send(url_list):
-    for link in url_list:
+    
+    for i, link in enumerate(url_list):
 
-        # OPEN BuzzMarker
+        print(f"{i+1}/{len(url_list)} Checking Conditions For {link}")
 
-        # Switch to BuzzMarker Iframe
-
+        has_not_been_logged_flag=False
         
+        driver.get(link)
+        pyautogui.hotkey("command", "0", interval=0.5)
+        
+        # Buzzstream_open = True
+
+        # CASE IF BUZZ STREAM HAS NOT LOGGED THIS CURRENT SITE
+        # FIRST VALIDATE EMAILS IF ALL EMAILS ARE VALID
+        # SECOND CLICK ON SAVE TO BUZZSTREAM BUTTON
+        # SINCE IT HAS LOGGED IN BUZZ STREAM SET LOGGED FLAG = FALSE
+        # (IF NOT FLAG) IGNORE DATE
+
+        try:
+            iframe = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.ID, "buzzstream-extension-form"))
+            )
+            driver.switch_to.frame(iframe)
+                
+            try:
+                save_buzzstream_button_exist = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, "//save-button[@ng-click='persistChanges()']/button"))
+                    
+                )
+                has_not_been_logged_flag = True
+            except Exception:
+                pass
+
+            if (has_not_been_logged_flag):
+                try: 
+                    email_list = WebDriverWait(driver, 3).until(
+                        EC.presence_of_all_elements_located((By.XPATH, "//contact-info-field[@placeholder='Email']//input[@type='text']"))
+                    )
+                    if (email_list): 
+                        save_buzzstream_button_exist.click()
+
+                except Exception:
+                    print("No Email List or No Valid Emails Found! ")
+                    pyautogui.hotkey("command", "0", interval=0.3)
+                    continue
+                
+            time.sleep(1)
 
 
+            # CASE CHECK TIME STAMP DATE
+            # IF IT HAS NOT BEEN MORE THAN TWO MONTHS AND NOT BEEN MARKED AS LOGGED: MOVE TO NEXT SITE
+
+            try:
+                WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, "//li[@ng-class=\"{ active: isCurrentPath('/history') }\"]")) 
+                ).click()
+            except Exception:
+                print("Cannot click history")
+
+            try:
+
+                try:
+                    time_date_stamp = WebDriverWait(driver, 5).until(
+                        EC.presence_of_element_located((By.XPATH,"//span[@class='chronicle-timestamp']"))
+                    ).text
+                except Exception:
+                    print("could not find time date stamp")
+                    pass
+                
+                if (not check_if_more_than_two_months(time_date_stamp) and not has_not_been_logged_flag):
+                    print(f"Last Date: {time_date_stamp} not been more than 2 Months or not. Skipping... ")
+                    pyautogui.hotkey("command", "0", interval=0.3)
+                    continue
+            except Exception:
+                pyautogui.hotkey("command", "0", interval=0.3)
+                continue
+
+            print(f"Last Date: {time_date_stamp} Moving Forward ")
+
+            # CHECK IF IN SEQUENCE
+            try:
+                WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, "//li[@ng-class=\"{ active: isCurrentPath('/qualify') }\"]"))
+                ).click()
+            except Exception:
+                pyautogui.hotkey("command", "0", interval=0.3)
+                continue
+        
+            try:
+                WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'card-right')]//button[contains(@class, 'btn-primary') and text()='Add']"))
+                ).click()
+            except Exception as e:
+                pass
+
+            try:
+                research_page_url_element = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.ID, "Research Page Url"))
+                )
+                research_page_url = research_page_url_element.get_attribute('value')
+
+                if (not research_page_url):
+                    pyautogui.hotkey("command", "0", interval=0.3)
+                    continue
+            except Exception as e:
+                pyautogui.hotkey("command", "0", interval=0.3)
+                continue
+
+            try:
+                WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//button[contains(@class, 'btn') and contains(@class, 'btn-link') and contains(., 'compose')]"))).click()
+            except Exception:
+                pyautogui.hotkey("command", "0", interval=0.3)
+                continue
+            
+            time.sleep(2)
+            mouse.position = (850, 880)
+            mouse.click(Button.left, 1)
+            time.sleep(2)
+
+            try:
+                WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, "//li[@ng-class=\"{ active: isCurrentPath('/') }\"]"))
+                ).click()
+            except Exception:
+                continue
+
+            driver.switch_to.default_content()
+            pyautogui.hotkey("command", "0", interval=0.3)
+            print(f"Successfully Sequenced For {link}")
+        
+        except Exception as e:
+            
+            print(f"An error occurred: {e}")
+           
+            continue
+            
+
+if (__name__ == '__main__'):
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-geolocation")
+    chrome_options.add_argument("--remote-allow-origins=*")
+    chrome_options.add_argument('user-data-dir=/Users/williamzhao/Library/Application Support/Google/Chrome/')
+    chrome_options.add_argument("--profile-directory=Profile 1")
+    chrome_options.add_argument("--start-maximized")
+    service = Service(executable_path='/opt/homebrew/bin/chromedriver')
+
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    action = ActionChains(driver)
+    mouse = Controller()
 
 
+    # mental-health-links, mental-health-resources, mental-health-links
+    # gov, no edu, huge websites
+    # Outbound links > 20
+    # 
+
+    search_queries =  [
+        # "psychological effects of being hit by a veichle -lawyer -firm inurl:resources OR inurl:links"
+        # "trauma after vehicular accidents -lawyer -firm",
+        "-firm -lawyer -law inurl:resources OR inurl:links psychological effects of vehicle accidents",
+        "-firm inurl:resources OR inurl:helpful-links -lawyer trauma after vehicular accidents",
+        "-firms -lawyer trauma after vehicular accidents",
+        "-firm post-traumatic stress disorder after car accidents -lawyer",
+        "-firm intext:emotional recovery after car accidents -lawyer",
+        "-firm inurl:support -lawyer trauma vehicular accidents",
+        "-firm -lawyer emotional impact of car accidents",
+        "-firm psychological effects after traffic accidents -lawyer",
+        "-firm intext:therapy -lawyer for car accident victims",
+        "-firm inurl:guides -lawyer trauma vehicular accidents",
+        "-firm -lawyer coping strategies after car accidents",
+        "-firm -lawyer trauma recovery following vehicular accidents",
+        "-firm -lawyer intext:mental health post car accident",
+        "-firm -lawyer inurl:articles trauma vehicular accidents",
+        "-firm -lawyer PTSD car accident survivors",
+        "-firm -lawyer long-term effects of car accident trauma",
+        "-firm -lawyer intext:support groups for accident victims",
+        "-firm -lawyer inurl:blogs trauma vehicular accidents",
+        "-firm -lawyer rehabilitation after car accidents",
+        "-firm -lawyer emotional trauma from road accidents",
+        "-firm -lawyer intext:counseling for car accident trauma",
+        "-firm -lawyer inurl:info trauma vehicular accidents",
+        "-firm -lawyer healing process after car accidents"
+    ]
 
 
+    for search_operator in search_queries:
+        try:
+            time.sleep(random.randrange(1, 5))
+            driver.get(f"https://www.google.com/search?q={search_operator}")
+            scroll_down()
+            urls = get_filter_urls(1, 31)
+            check_and_send(urls)
+        except Exception:
+            continue
 
-driver.get("https://www.google.com/search?q=boat-ed.com+inurl:resources")
-scroll_down()
-highlight_contacts()
-urls = get_all_urls()
+    #https://www.providencetherapybc.com/resources
+    #https://braininjuryconnectionsnw.org/resources/health-care-and-alternative-medical-practices/counselors-and-mental-health/
+    # check_and_send(["https://www.goshen1.org/resources/parents/mental_health_resources.php"])
+    
 
-
-time.sleep(9999)
-driver.quit()
+    time.sleep(9999)
+    driver.quit()
